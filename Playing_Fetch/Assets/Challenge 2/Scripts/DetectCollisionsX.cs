e534ddb97f00e4ac5e72c017727d41a6280fbb9d
Grace Christensen <10885182@my.uvu.edu>
2021-04-12 17:33:20 -0600

    lost files found, moved into right place

---
+++ Playing_Fetch/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //destroy ball when in contact with other ie. dog
+        Destroy(gameObject);
+    }
+}
---
+++ Prototype_1/Assets/Script/Playermovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+  //Access Modifier, Data Type, Name
+  public float speed = 30.0f;
+  public float turnspeed =35.0f;
+  
+  public float hInput;
+  public float fInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    // Gathers the inputs for the controls
+      hInput = Input.GetAxis("Horizontal");
+      fInput = Input.GetAxis("Vertical");
+    // Makes the vehicle go forward and back
+      transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
+    // Makes the vehicle go left and right
+      transform.Rotate(Vector3.up,turnspeed * hInput * Time.deltaTime);
+
+    }
+}
