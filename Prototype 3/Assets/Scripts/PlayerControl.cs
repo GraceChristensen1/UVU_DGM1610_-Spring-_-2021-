52c8516ae9c3892e2eea8bcf7e68f011a6f2f9b4
Grace Christensen <10885182@my.uvu.edu>
2021-04-13 19:38:55 -0600

    Code got messed up went back to 3.1

---
+++ Prototype 3/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpforce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerRb = GetComponent<Rigidbody>();  
+      Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+      {
+          playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+          isOnGround = false;
+      }  
+    }
+
+    private void OnCollisionEnter(Collision Collision)
+    {
+
+        if(Collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if(Collision.gameObject.CompareTag("Obstacle"))
+        {   
+        Debug.Log("Game Over!");
+         gameOver = true;
+        }
+    }
+}
